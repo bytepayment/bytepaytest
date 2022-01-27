5a5cfda18ca28e618551841ab9dab10d04ed8bf9
lunaragon <iterator_sl@qq.com>
2022-01-28 01:00:41 +0800

    init tests

---
+++ tests/config.js
@@ -0,0 +1,25 @@
+const request = require("./request");
+
+async function getToken() {
+  const r = await request.post("get_token_for_test");
+  return r.data;
+}
+
+async function getDevToken() {
+  const r = await request.post("get_developer_token_for_test");
+  return r.data;
+}
+
+module.exports = {
+  getToken,
+  getDevToken,
+  test_user_id: 37979965,
+  test_dev_id: 23449728,
+  test_user_login: "sulnong",
+  test_org_name: "bytepayment",
+  test_repo_name: "bytepaytest",
+  test_repo_id: 138711724,
+  test_user_polka_address: "5GbNZj4ntEt25oMrZTfpXHfwVQhvJmb8vBX33mj7L7v7P3EK",
+  test_recv_address: "5FU6HZLVj74hF1sBxAFQ7PkS6HQbVF3gAFDLPLV37YFNKNjV",
+  test_bind_own_address: "5FU6HZLVj74hF1sBxAFQ7PkS6HQbVF3gAFDLPLV37YFNKNjV",
+};
